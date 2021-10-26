c7e6fe7c489b42da319740c4ed016e74db43e1ba
LepkoQQ <lepko.san@gmail.com>
2021-10-26 19:27:47 +0200

    add api models and register them in admin

--- novdan_api/api/admin.py
+++ novdan_api/api/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.db.models import Q
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Wallet, Subscription, SubscriptionTimeRange, Transaction
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'amount')
+    list_filter = ('user__is_staff',)
+    search_fields = ('id', 'user__username')
+
+
+class IsSubscriptionActiveFilter(admin.SimpleListFilter):
+    title = 'is active'
+    parameter_name = 'is_active'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', _('Yes')),
+            ('0', _('No')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '1':
+            return queryset.filter(
+                Q(active_range__started_at__lte=timezone.now()),
+                Q(active_range__ended_at__gte=timezone.now()) | Q(active_range__ended_at__isnull=True),
+            )
+        if self.value() == '0':
+            return queryset.exclude(
+                Q(active_range__started_at__lte=timezone.now()),
+                Q(active_range__ended_at__gte=timezone.now()) | Q(active_range__ended_at__isnull=True),
+            )
+        return queryset
+
+
+class SubscriptionTimeRangeStackedInline(admin.StackedInline):
+    model = SubscriptionTimeRange
+    readonly_fields = ('started_at',)
+    fields = ('started_at', 'ended_at')
+    extra = 0
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'is_active')
+    list_filter = ('user__is_staff', IsSubscriptionActiveFilter)
+    search_fields = ('id', 'user__username')
+    inlines = [SubscriptionTimeRangeStackedInline]
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
--- novdan_api/api/models.py
+++ novdan_api/api/models.py
@@ -1,3 +1,64 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.db.models import Q
+from django.utils import timezone
+
+
+class Wallet(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    amount = models.BigIntegerField(default=0)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'Wallet ({self.id})'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    @property
+    def is_active(self):
+        return self.active_ranges.filter(
+            Q(started_at__lte=timezone.now()),
+            Q(ended_at__gte=timezone.now()) | Q(ended_at__isnull=True),
+        ).exists()
+
+    def __str__(self):
+        return f'Subscription ({self.id})'
+
+
+class SubscriptionTimeRange(models.Model):
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(blank=True, null=True)
+    subscription = models.ForeignKey(
+        Subscription,
+        on_delete=models.CASCADE,
+        related_name="active_ranges",
+        related_query_name="active_range",
+    )
+    # TODO: add some kind of payment id from "podpri" api
+
+
+class Transaction(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    amount = models.BigIntegerField()
+    from_wallet = models.ForeignKey(
+        Wallet,
+        on_delete=models.CASCADE,
+        related_name="outgoing_transactions",
+        related_query_name="outgoing_transaction",
+    )
+    to_wallet = models.ForeignKey(
+        Wallet,
+        on_delete=models.CASCADE,
+        related_name="incoming_transactions",
+        related_query_name="incoming_transaction",
+    )
