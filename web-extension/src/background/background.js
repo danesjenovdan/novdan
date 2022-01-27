b22dad5b2d08752c1215c39cb72e3cd11306ec0f
LepkoQQ <lepko.san@gmail.com>
2022-01-27 14:00:59 +0100

    open dash on extension button click

--- web-extension/src/background/background.js
+++ web-extension/src/background/background.js
@@ -1,1 +1,11 @@
-console.log("Background started!");
+console.log('Background started!');
+
+browser.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    browser.tabs.create({ url: 'https://novdan.si/dash' });
+  }
+});
+
+browser.browserAction.onClicked.addListener((tab) => {
+  browser.tabs.create({ url: 'https://novdan.si/dash' });
+});
