070bb92f182cbb7ccbcef3cb0f45d080312f6b2a
Filip Muki Dobranić <filipdobranic@gmail.com>
2022-02-09 08:36:19 +0100

    set up staging on k8s

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
 # BUILD AND PUBLISH NOVDAN FRONTEND
-sudo docker build -f novdan_frontend/Dockerfile -t novdan-frontend:latest ./novdan_frontend
-sudo docker tag novdan-frontend:latest rg.fr-par.scw.cloud/djnd/novdan-frontend:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/novdan-frontend:latest
+sudo docker build -f novdan_frontend/Dockerfile -t novdan-frontend-staging:latest ./novdan_frontend
+sudo docker tag novdan-frontend-staging:latest rg.fr-par.scw.cloud/djnd/novdan-frontend-staging:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/novdan-frontend-staging:latest
 
 # BUILD AND PUBLISH NOVDAN API
-sudo docker build -f novdan_api/Dockerfile -t novdan-api:latest ./novdan_api
-sudo docker tag novdan-api:latest rg.fr-par.scw.cloud/djnd/novdan-api:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/novdan-api:latest
+sudo docker build -f novdan_api/Dockerfile -t novdan-api-staging:latest ./novdan_api
+sudo docker tag novdan-api-staging:latest rg.fr-par.scw.cloud/djnd/novdan-api-staging:latest
+sudo docker push rg.fr-par.scw.cloud/djnd/novdan-api-staging:latest
