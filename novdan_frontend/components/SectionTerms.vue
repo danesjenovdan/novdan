2a146a6507d48849a036a08fb6217b30f5518bf2
muki <filipdobranic@gmail.com>
2022-02-17 08:53:35 +0100

    add terms and conditions

---
+++ novdan_frontend/components/SectionTerms.vue
@@ -0,0 +1,299 @@
+<template>
+  <section class="background-black faq">
+    <div class="container">
+      <div>
+        <h1>Splošni pogoji uporabe</h1>
+        <h4>SPLOŠNE DOLOČBE</h4>
+        <hr />
+        <p>
+          Lastnik spletnega mesta in brskalniškega vtičnika »Nov dan« je Danes
+          je nov dan, Inštitut za druga vprašanja, Parmova ulica 20, 1000
+          Ljubljana; matična številka: 6468594000 (v nadaljevanju Upravljalec).
+        </p>
+        <p>
+          Uporabo spletnega mesta Nov dan (brskanje, odpiranje povezav, branje
+          vsebine in podobno) in brskalniških vtičnikov za Chrome in Firefox Nov
+          dan štejemo kot soglasje obiskovalca in uporabnika, da se strinja s
+          temi splošnimi pogoji uporabe. V kolikor se kot obiskovalec in
+          uporabnik ne strinjate s temi splošnimi pogoji, vas prosimo, da
+          spletnega mesta in brskalniških vtičnikov ne uporabljate.
+        </p>
+        <p>
+          Ti splošni pogoji veljajo za celotno spletno mesto in brskalniška
+          vtičnika za Chrome in Firefox, v kolikor za posamezen del izrecno ni
+          določeno drugače.
+        </p>
+        <p>
+          Namen tega obvestila je, da vam pomaga razumeti delovanje sistema
+          donacij ter politike varovanja osebnih podatkov. Pridržujemo si
+          pravico do spremembe pogojev, zato jih občasno preverite. V primeru
+          kakršnih koli nejasnosti se prosimo obrnite na katero od spodnjih
+          kontaktnih možnosti.
+        </p>
+        <ul>
+          <li>
+            <a href="mailto:vsi@danesjenovdan.si">vsi@danesjenovdan.si</a>
+          </li>
+          <li>
+            <a href="tel:0038664147823">+386 64 147 823</a> (vsak delovni dan od
+            10. do 16. ure)
+          </li>
+          <li>
+            Danes je nov dan, Inštitut za druga vprašanja, Parmova ulica 20,
+            1000 Ljubljana
+          </li>
+        </ul>
+        <h4>PODPORNIKI</h4>
+        <hr />
+        <h5>Načini plačila</h5>
+        <p>
+          Podporniki lahko na platformi Nov dan podprejo upravičence z mesečno
+          podporo (donacijo), ki jo poravnajo s plačilno kartico na spletu (prek
+          sistema za zagotavljanja plačil Braintree) ali prek storitve PayPal.
+        </p>
+        <h5>Izvedba plačila</h5>
+        <p>
+          Donacija bo avtomatsko trgana vsakega 1. v mesecu. Po uspešno izvedeni
+          transakciji bo podpornik na svoj e-naslov prejel potrdilo.
+        </p>
+        <h5>Postopek prekinitve mesečne podpore</h5>
+        <p>
+          Prekinitev mesečne podpore lahko podpornik opravi v nadzorni plošči,
+          ki je po registraciji in inštalaciji vtičnika dostopna na
+          <a href="https://novdan.si/dash/">https://novdan.si/dash/</a>, in
+          sicer s klikom na gumb “Prekini naročnino”, ali pa z odjavo prek
+          elektronskega sporočila na
+          <a href="mailto:novdan@danesjenovdan.si">novdan@danesjenovdan.si</a>.
+        </p>
+        <h4>UPRAVIČENOST DO PODPORE</h4>
+        <hr />
+        <h5>Upravičenec</h5>
+        <p>
+          Upravičenec do zbranih sredstev je vsaka organizacija, ki producira
+          medijske vsebine, in je navedena na spletnem mestu Nov dan v razdelku
+          Sodelujoči mediji.
+        </p>
+        <h5>Predaja sredstev</h5>
+        <p>
+          Upravljalec bo sredstva, zbrana na platformi Nov dan, ki jih bo
+          upravljalec prejel na svoj transakcijski ali PayPal račun, predal
+          upravičencem, ki so navedeni na platformi Nov dan. Sredstva bodo med
+          upravičence razdeljena glede na čas, ki so ga podporniki preživeli na
+          spletnih mestih upravičencev, ki so opremljena z WMS meta tagom.
+        </p>
+        <p>
+          Upravljalec je upravičen do kritja stroškov provizije, ki nastanejo
+          pri transakcijah sredstev podpornikov, v višini 3 % vseh zbranih
+          sredstev.
+        </p>
+        <h5>Odvzem sredstev</h5>
+        <p>
+          V primeru, da upravljalec naknadno ugotovi, da je upravičenec sredstva
+          pridobil na podlagi neresničnih navedb ali podatkov, mu upravljalec
+          zbranih sredstev ne bo izplačal. V primeru, da so bila sredstva že
+          nakazana, je upravičenec dolžan zbrana sredstva vrniti na račun
+          upravljalca.
+        </p>
+        <h5>Prerazporeditev zbranih sredstev</h5>
+        <p>
+          V primeru vračanja že izplačanih sredstev, ali v primeru, ko sredstva
+          na podlagi predhodnega člena sploh niso izplačana, upravljalec v roku
+          1 meseca ta sredstva enakomerno porazdeli med preostale sodelujoče na
+          platformi Nov dan.
+        </p>
+        <h4>OMEJITEV ODGOVORNOSTI PRI UPORABI SPLETNE STRANI IN APLIKACIJ</h4>
+        <hr />
+        <p>
+          Obiskovalci spletne strani uporabljajo spletno mesto, njene dele in
+          brskalniške vtičnike izključno na lastno odgovornost.
+        </p>
+        <p>
+          Upravljalec ne prevzema nikakršnih odgovornosti o točnosti in
+          pravilnosti na spletnem mestu objavljenih informacij, ne prevzema
+          odgovornosti zaradi spremembe že objavljenih informacij in ne prevzema
+          odgovornosti za morebitno nastalo škodo, ki bi obiskovalcu spletnega
+          mesta ali vtičnikov strani nastala zaradi uporabe tam objavljenih
+          informacij.
+        </p>
+        <p>Upravljalec ne odgovarja za nadomestilo:</p>
+        <ol>
+          <li>
+            morebitne neposredne, posredne, naključne, posebne, kaznovalne,
+            posledične škode, ali druge kakršne koli druge oblike škode, ki
+            posredno, neposredno ali na kakršenkoli drug način izhaja iz napak,
+            pomot ali netočnosti vsebine spletnega mesta in vtičnikov, ali iz
+            kakršnih koli drugih dejstev povezanih s spletnim mestom, vtičnikoma
+            ali njihovo uporabo,
+          </li>
+          <li>
+            kakršnekoli škode, ki nastane zaradi nepooblaščenega dostopa do, ali
+            uporabe, naših strežnikov in/ali katerihkoli in vseh v njih
+            shranjenih podatkov.
+          </li>
+        </ol>
+        <p>
+          Upravljalec ne prevzema nikakršnih odgovornosti za vsebine na spletnih
+          mestih in drugih kanalih upravičencev.
+        </p>
+        <h4>VARNOST</h4>
+        <hr />
+        <p>
+          Upravljalec verjame, da je varnost podpornikov izjemno pomembna. V ta
+          namen je upravljalec implementiral 3-D Secure tehnologijo, ki poskrbi,
+          da je postopek doniranja še bolj varen.
+        </p>
+        <p>
+          3-D Secure je sistem, ki vključuje tehnične specifikacije in zahteve
+          za izdajatelje kartic, uporabnike kartic ter trgovce oziroma ponudnike
+          storitev. Na kratko, gre za dodatno metodo preverjanja pristnosti za
+          transakcije s kreditnimi karticami, ki potekajo na spletu. Poleg tega,
+          da se za zaščito informacij o plačilni kartici med prenosom preko
+          spleta uporablja široko podprta spletna tehnologija za kodiranje
+          Secure Socket Layer (SSL), 3-D Secure avtenticira vse vključene strani
+          v transakciji ter zagotavlja zaščito pred zlonamerno uporabo kartice,
+          kar pomeni, da je za vsako transakcijo preverjena identiteta vseh
+          vpletenih v proces, imetnika plačilne kartice, trgovca (spletno
+          prodajno mesto) in banke.
+        </p>
+        <p>Tehnologija pripomore k varnosti na dveh ključnih korakih:</p>
+        <ul>
+          <li>
+            Preverjanje istovetnosti lastnika kartice: 3-D Secure po vnosu in
+            potrditvi klasičnih kartičnih podatkov v dodatnem oknu zahteva tudi
+            vpis enkratnega gesla, ki se ga uporabniku pošlje v SMS sporočilu
+            ali preko aplikacije za mobilno bančništvo. Transakcija bo potrjena
+            in izvedena šele, ko bo vneseno geslo pravilno.
+          </li>
+          <li>
+            Preverjanje avtentičnosti spletnega mesta: 3-D Secure preverja
+            spletna mesta ter zagotavlja njihovo avtentičnost ter varnost
+            opravljenih transakcij.
+          </li>
+        </ul>
+        <p>
+          Poleg 3-D Secure tehnologije za varnost transakcij skrbi tudi SSL
+          certifikat: digitalno potrdilo, ki omogoča kodiranje podatkov med
+          strežnikom in odjemalcem preko varnega (šifriranega oziroma
+          kodiranega) protokola in s tem zagotavlja visoko stopnjo varnosti
+          komunikacije.
+        </p>
+        <h4>VAROVANJE OSEBNIH PODATKOV</h4>
+        <hr />
+        <h5>Razlogi za zbiranje in raba osebnih podatkov</h5>
+        <p>
+          Upravljalec je predan varovanju osebnih podatkov in transparentni rabi
+          podatkov in se se zavezuje, da bo podatke uporabljal v skladu z vsemi
+          veljavnimi zakoni o varstvu osebnih podatkov.
+        </p>
+        <p>
+          Upravljalec bo osebne podatke uporabil izključno za potrebe izpolnitve
+          naročila: elektronski naslov za komunikacijo s podpornikov in podatke
+          plačilne kartice za izvedbo plačila. Upravljalec bo elektronski naslov
+          hranil skladno z veljavno zakonodajo in ne več kot 1 leto po
+          prekinitvi naročnine. Upravljalec podatkov plačilne kartice ne hrani.
+        </p>
+        <p>
+          Podatki ne bodo v nobenem primeru posredovani nepooblaščenim osebam.
+          Za zagotavljanje plačila upravljalec posreduje podatke o plačilni
+          kartici partnerju, ki upravlja s sistemom zagotavljanja plačil:
+          Braintree, PayPal (Europe) S.à r.l. et Cie, S.C.A.
+        </p>
+        <h5>Pojasnilo o vaših pravicah glede varstva osebnih podatkov</h5>
+        <p>
+          Uporabnik lahko zahteva izvoz in vpogled do osebnih podatkov, ki jih
+          upravljalec hrani o uporabniku, s posredovanjem pisne zahteve na
+          e-naslov
+          <a href="mailto:novdan@danesjenovdan.si">novdan@danesjenovdan.si</a>.
+          Osebni podatki so shranjeni v posebni aplikaciji, ki spoštuje zahteve
+          zasebnosti ter si za najvišjo varnost hranjenih podatkov prizadeva z
+          različnimi fizičnimi, tehničnimi in organizacijskimi ukrepi.
+        </p>
+        <hr />
+        <p>V Ljubljani, 7. 2. 2022</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.faq {
+  padding: 3rem 30px;
+
+  @media (min-width: 1200px) {
+    padding: 6rem 0;
+
+    .container > div {
+      padding-left: 8rem;
+      padding-right: 6rem;
+    }
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 800;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    color: #ff5ccb;
+
+    @media (min-width: 992px) {
+      font-size: 6rem;
+    }
+
+    @media (min-width: 1200px) {
+      font-size: 8rem;
+    }
+  }
+
+  h4,
+  h5,
+  p,
+  ul,
+  li {
+    color: #dddddd;
+
+    @media (min-width: 992px) {
+      margin-right: 200px;
+    }
+  }
+
+  h4 {
+    font-size: 1.5rem;
+    font-weight: 800;
+    margin-bottom: 1.5rem;
+
+    @media (min-width: 992px) {
+      font-size: 2rem;
+    }
+  }
+  h5 {
+    font-size: 1.2rem;
+    font-weight: 800;
+    margin-bottom: 1.2rem;
+
+    @media (min-width: 992px) {
+      font-size: 1.8rem;
+    }
+  }
+
+  p,
+  ul,
+  li {
+    font-size: 1.25rem;
+
+    @media (min-width: 992px) {
+      font-size: 1.5rem;
+    }
+  }
+
+  hr {
+    background-color: #ffd700;
+    border-color: #ffd700;
+    width: 8rem;
+    margin: 0;
+  }
+
+  a {
+    color: #dddddd;
+  }
+}
+</style>
