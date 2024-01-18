a95fb170df670c732a8d2a71e923ec2ac6547f3e
LepkoQQ <lepko.san@gmail.com>
2024-01-18 11:16:32 +0100

    remove zdravo section on dash

--- novdan_frontend/components/DashIntro.vue
+++ novdan_frontend/components/DashIntro.vue
@@ -2,7 +2,7 @@
   <section class="background-white what">
     <div class="container">
       <div class="content-narrow">
-        <p class="text-big">
+        <!-- <p class="text-big">
           <span>ZDRAVO!</span>
         </p>
         <p class="text-small">
@@ -12,7 +12,7 @@
           <a href="mailto:novdan@danesjenovdan.si" target="_blank">nam piši</a>.
         </p>
         <div class="yellow-bg" />
-        <hr v-if="isSubscribed" />
+        <hr v-if="isSubscribed" /> -->
         <div class="row">
           <div v-if="isSubscribed" style="position: relative">
             <h3>Tvoja razporeditev</h3>
@@ -104,15 +104,9 @@ export default {
     position: relative;
   }
 
-  // h3 {
-  //   font-size: 3rem;
-  //   font-weight: 400;
-  //   font-family: 'Le Murmure';
-  //   text-transform: uppercase;
-  //   letter-spacing: 3px;
-  //   margin-top: 0;
-  //   margin-bottom: 2rem;
-  // }
+  h3 {
+    margin-top: 5rem;
+  }
 
   p {
     @media (min-width: 1200px) {
